38cdf966aeaeccfcdacb702503890b2630c537ae
Ulrich J. Beutenmüller <uli@beutenmueller.de>
2025-06-26 18:41:00 +0200

    added a few more scripts

---
+++ mount-dirs.ps1
@@ -0,0 +1,3 @@
+net use m: \\10.95.60.101\management /persistent:yes /user:uli
+net use l: \\10.95.60.101\lab /persistent:yes /user:uli
+net use q: \\10.95.60.101\qualitymanagement /persistent:yes /user:uli
